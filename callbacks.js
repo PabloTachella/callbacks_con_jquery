1bdd3635ca0e8a029c552d4124cf119cb9e14496
PabloTachella <pablotachella95@gmail.com>
2021-04-22 18:27:09 -0300

    Modificacion del archivo callbacks.js para realizar multiples requests con una funcion y poner a prueba el funcionamiento del asincronismo en js

--- callbacks.js
+++ callbacks.js
@@ -1,8 +1,19 @@
 const URL_API = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id/'
 
-const URL = `${URL_API}${PEOPLE_URL.replace(':id', 1)}`
+
 const OPTIONS = { crossDomain: true }
 let onPeopleResponse = function (people) { console.log(`Hola, me llamo ${people.name}`) }
 
-$.get( URL, OPTIONS, onPeopleResponse)
+function obtenerPersonaje(id) {
+    const URL = `${URL_API}${PEOPLE_URL.replace(':id', id)}`
+    $.get( URL, OPTIONS, onPeopleResponse)
+}
+
+obtenerPersonaje(1)
+obtenerPersonaje(2)
+obtenerPersonaje(3)
+
+//Por el asincronismo de JS. No sabemos en que orden nos llegaran las respuestas, 
+//esto depende del servidor y de cada uno de los requests. Iniciamos los requests en un 
+//determinado orden pero no sabemos en que orden van a llegar.
