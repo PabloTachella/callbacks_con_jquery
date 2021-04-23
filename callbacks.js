8b79030c3fbc342c1560cd8bbd1638e5bcea1200
PabloTachella <pablotachella95@gmail.com>
2021-04-23 15:01:05 -0300

    Se realiza la misma tarea que hasta el momento pero ahora con async await, se dejan notas de como funciona la parte asincrona

--- callbacks.js
+++ callbacks.js
@@ -8,30 +8,32 @@ function onError(id){
 }
 
 function obtenerPersonaje(id) {
+
     return new Promise( function( resolve, reject ) {
-
 		const URL = `${URL_API}${PEOPLE_URL.replace(':id', id)}`
-
-        $.get( URL, OPTIONS, function (data) {resolve(data)})
-        //con los parametros URL y OPTIONS ya tengo lo necesario para realizar el request, si este tiene
-        //exito, el objeto que obtengo como respuesta lo recibo como parametro en mi callback
-        //(en este caso "data")... mi callback ejecuta la funcion resolve(data) apuntando a .then() 
-
-        .fail(() => reject(id))
-        //Si por algun motivo el request no se concreta correctamente se ejecutara reject()
-        //dicha funcion apunta al .catch()
-    })
+        $
+            .get( URL, OPTIONS, function (data) {resolve(data)})
+            .fail(() => reject(id))
+    })  
 }
 
-var ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+async function obtenerPersonajes () {
 
-var promesas = ids.map(id => obtenerPersonaje(id))
-Promise
-    .all(promesas)
-    //.all() recibe un array de promesas como parametro
-    .then(personaje => console.log(personaje))
-    //el parametro personaje que esta recibiendo .then es un array de objetos con los 10 primeros personajes
-    //podria manipularlo asi .then(personaje => console.log(personaje[0].name)) e imprimiria el nombre del 1er personaje
-    .catch(onError)
+    var ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    var promesas = ids.map(id => obtenerPersonaje(id))
+
+    try {
+        var personajes = await Promise.all(promesas)
+        //lo que hace await es pausar el flujo de esta funcion, en espera de que todas las promesas se resuelvan
+        //una vez que todas las promesas se resuelven guarda ese resultado en la variable personajes
+        //esto sucede de forma asincrona, es decir, cuando se pausa el flujo de esta funcion, mientras se
+        //espera la resolucion de todas las promesas, el flujo principal JS se sigue ejecutando
+        console.log(personajes)
+    } catch (id) {
+        onError(id)
+    }
+}
+
+obtenerPersonajes()
 
     
